c809a65e382fbf156314297c2c3b90a14f8fbd4c
Alan Malloy <alan@malloys.org>
2017-12-06 05:11:36 +0000

    Day 1, part 1

--- aoc-day01/src/Main.hs
+++ aoc-day01/src/Main.hs
@@ -1,5 +1,11 @@
 module Main where
+import Data.Char (digitToInt)
+
+pairs :: String -> [(Char, Char)]
+pairs s = zip s (tail s ++ [head s])
+
+part1 :: [(Char, Char)] -> Int
+part1 ps = sum [digitToInt a | (a, b) <- ps, a == b]
 
 main :: IO ()
-main = do
-  putStrLn "hello world"
+main = interact (show . part1 . pairs . filter (/= '\n'))
