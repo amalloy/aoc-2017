3977f3f8b1b54c700d0c3405801e7d389a85be9c
Alan Malloy <alan@malloys.org>
2017-12-06 19:24:34 +0000

    Day 3, part 2. What a gross implementation.

--- aoc-day03/src/Main.hs
+++ aoc-day03/src/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import Prelude hiding (Left, Right)
 
+import Control.Arrow ((&&&))
 import Control.Monad.Trans.State.Lazy
 import qualified Data.Map.Strict as M
 
@@ -27,7 +28,8 @@ spiral label result = go
   where go = do
           newValue <- label
           (Grid i dir pos m) <- get
-          put $ Grid i dir pos (M.insert pos newValue m)
+          let m' = (M.insertWith (flip const) pos newValue m)
+          put $ Grid i dir pos m'
           done <- result
           case done of
             Just x -> return x
@@ -35,15 +37,15 @@ spiral label result = go
               let turned = turnLeft dir
                   left = move turned pos
                   forward = move dir pos
-                  (dir', pos') = if M.member left m
+                  (dir', pos') = if M.member left m'
                                  then (dir, forward)
                                  else (turned, left)
-              put $ Grid (i + 1) dir' pos' (M.insert pos i m)
+              put $ Grid (i + 1) dir' pos' m'
               go
 
 solve :: State MapGrid Int -> State MapGrid (Maybe a) -> a
 solve label result = evalState (spiral label result)
-                     (Grid 1 Down (Pos 0 0) M.empty)
+                     (Grid 1 Down (Pos 0 0) $ M.fromList [(Pos 0 0, 1)])
 
 part1 :: Int -> Int
 part1 goal = solve label result
@@ -56,5 +58,21 @@ part1 goal = solve label result
             then return $ Just (abs x + abs y)
             else return Nothing
 
+part2 :: Int -> Int
+part2 goal = solve label result
+  where label = do
+          (Grid _ _ pos@(Pos y x) m) <- get
+          return . sum $ do
+            (dy, dx) <- tail [(a,b) | a <- [0,1,-1], b <- [0,1,-1]]
+            let pos' = (Pos (y + dy) (x + dx))
+                val = M.findWithDefault 0 pos' m
+            return val
+        result = do
+          (Grid _ _ pos m) <- get
+          let curr = m M.! pos
+          if curr > goal
+            then return (Just curr)
+            else return Nothing
+
 main :: IO ()
-main = interact $ show . part1 . read
+main = interact $ show . (part1 &&& part2) . read
