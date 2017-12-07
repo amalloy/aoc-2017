073628177ce407f5e92ef6c02126617d36f60f6e
Alan Malloy <alan@malloys.org>
2017-12-07 02:04:21 +0000

    Day 4, part 2

--- aoc-day04/src/Main.hs
+++ aoc-day04/src/Main.hs
@@ -1,10 +1,14 @@
 module Main where
 
-import Data.List (nub)
+import Data.List (nub, sort)
+import Control.Arrow ((&&&))
 
 part1 :: [[String]] -> Int
 part1 = length . filter permissible
   where permissible xs = xs == nub xs
 
+part2 :: [[String]] -> Int
+part2 = part1 . map (map sort)
+
 main :: IO ()
-main = interact $ show . part1 . map words . lines
+main = interact $ show . (part1 &&& part2) . map words . lines
