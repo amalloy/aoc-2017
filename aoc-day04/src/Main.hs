74b1dddbdc85a25965e481d55a37b5e6bacad01e
Alan Malloy <alan@malloys.org>
2017-12-07 01:58:13 +0000

    Day 4, part 1

--- aoc-day04/src/Main.hs
+++ aoc-day04/src/Main.hs
@@ -1,5 +1,10 @@
 module Main where
 
+import Data.List (nub)
+
+part1 :: [[String]] -> Int
+part1 = length . filter permissible
+  where permissible xs = xs == nub xs
+
 main :: IO ()
-main = do
-  putStrLn "hello world"
+main = interact $ show . part1 . map words . lines
