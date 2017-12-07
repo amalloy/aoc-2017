0148a858409505dd4df0a8df9875bcb6443a1a74
Alan Malloy <alan@malloys.org>
2017-12-07 05:53:22 +0000

    Day 6, part 1

--- aoc-day06/src/Main.hs
+++ aoc-day06/src/Main.hs
@@ -1,5 +1,35 @@
 module Main where
 
+import Data.List (maximumBy)
+import Data.Ord (comparing, Down(..))
+
+data EvacOrder = Evac Int Int
+
+iterations :: (a -> a) -> a -> [[a]]
+iterations f x = go [x]
+  where go all@(curr:prev) = all : go (f curr : all)
+
+target :: [Int] -> EvacOrder
+target xs = let (sz, (Down idx)) = (maximum $ zip xs (map Down [0..]))
+            in Evac idx sz
+
+shuffle :: [Int] -> [Int]
+shuffle xs = let t@(Evac bank size) = target xs
+                 len = length xs
+                 resize i amt | i == bank = size `div` len
+                              | otherwise = size `div` len + amt + leftover
+                   where leftover | size `mod` len >= (i - bank) `mod` len = 1
+                                  | otherwise = 0
+             in zipWith resize [0..] xs
+
+unique :: Eq a => [a] -> Bool
+unique [] = True
+unique (x:xs) = not $ x `elem` xs
+
+part1 :: [Int] -> Int
+part1 banks = let allStates = iterations shuffle banks
+                  repetitions = dropWhile unique allStates
+              in length (head repetitions) - 1
+
 main :: IO ()
-main = do
-  putStrLn "hello world"
+main = interact $ show . part1 . map read . words . head . lines
