a9c0f75b4dad9afc4756d822dcd2773bcaf32b23
Alan Malloy <alan@malloys.org>
2017-12-08 03:21:27 +0000

    Day 7, part 1

--- aoc-day07/src/Main.hs
+++ aoc-day07/src/Main.hs
@@ -1,12 +1,40 @@
 module Main where
 
 import Control.Arrow ((&&&))
+import qualified Data.Map.Lazy as M
+import Data.List (maximumBy)
+import Data.Ord (comparing)
 
-part1 :: a -> ()
-part1 = const ()
+data Line = Line String Int [String] deriving Show
+data Tree a = Node {nodeName :: String,
+                    nodeWeight :: Int,
+                    nodeSize :: Int,
+                    nodeMeta :: a,
+                    nodeChildren :: [Tree a]}
+            deriving Show
 
-part2 :: a -> ()
-part2 = const ()
+parse :: [String] -> Line
+parse (name:weight:more) = Line name (read weight) $ case more of
+  [] -> []
+  ("->":children) -> map (filter (/= ',')) children
+
+buildTree :: [Line] -> M.Map String (Tree ())
+buildTree lines = t
+  where t = M.fromList $ do
+          (Line name weight children) <- lines
+          let childNodes = map (t M.!) children
+              size = 1 + sum (map nodeSize childNodes)
+          return (name, Node name weight size () $ childNodes)
+
+root :: M.Map String (Tree a) -> Tree a
+root = maximumBy (comparing nodeSize) . M.elems
+
+part1 :: Tree a -> String
+part1 = nodeName
+
+part2 :: Tree a -> Int
+part2 t = undefined
 
 main :: IO ()
-main = interact $ show . (part1 &&& part2) . lines
+main = interact $ show . (part1 &&& part2) .
+       root . buildTree . map (parse . words) . lines
