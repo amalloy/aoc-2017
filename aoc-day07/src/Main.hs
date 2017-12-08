264d6eaeefd51060023eb55b53c0ef897ea323cb
Alan Malloy <alan@malloys.org>
2017-12-08 04:36:22 +0000

    Day 7, part 2

--- aoc-day07/src/Main.hs
+++ aoc-day07/src/Main.hs
@@ -2,7 +2,7 @@ module Main where
 
 import Control.Arrow ((&&&))
 import qualified Data.Map.Lazy as M
-import Data.List (maximumBy)
+import Data.List (maximumBy, sort, nub)
 import Data.Ord (comparing)
 
 data Line = Line String Int [String] deriving Show
@@ -18,13 +18,14 @@ parse (name:weight:more) = Line name (read weight) $ case more of
   [] -> []
   ("->":children) -> map (filter (/= ',')) children
 
-buildTree :: [Line] -> M.Map String (Tree ())
+buildTree :: [Line] -> M.Map String (Tree Int)
 buildTree lines = t
   where t = M.fromList $ do
           (Line name weight children) <- lines
           let childNodes = map (t M.!) children
               size = 1 + sum (map nodeSize childNodes)
-          return (name, Node name weight size () $ childNodes)
+              totalWeight = weight + sum (map nodeMeta childNodes)
+          return (name, Node name weight size totalWeight $ childNodes)
 
 root :: M.Map String (Tree a) -> Tree a
 root = maximumBy (comparing nodeSize) . M.elems
@@ -32,8 +33,18 @@ root = maximumBy (comparing nodeSize) . M.elems
 part1 :: Tree a -> String
 part1 = nodeName
 
-part2 :: Tree a -> Int
-part2 t = undefined
+part2 :: Tree Int -> (String, Int)
+part2 root@(Node _ w _ _ nodes) = let weights = sort . map nodeMeta $ nodes
+                                      goalWeight = w + (length nodes *
+                                                        (weights !! 1))
+                             in interrogate root goalWeight
+
+interrogate :: Tree Int -> Int -> (String, Int)
+interrogate t@(Node n w _ tw children) goal
+  | length (nub childWeights) <= 1 = (n, goal - sum childWeights)
+  | otherwise = interrogate (head [child | child <- children, childGoal /= nodeMeta child]) childGoal
+  where childWeights = map nodeMeta children
+        childGoal = (goal - w) `div` length children
 
 main :: IO ()
 main = interact $ show . (part1 &&& part2) .
