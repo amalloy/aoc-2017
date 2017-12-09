03189028d77f23a2598bdad345e697daf369b61c
Alan Malloy <alan@malloys.org>
2017-12-09 05:24:51 +0000

    Day 9, replace part 1 with part 2

--- aoc-day09/src/Main.hs
+++ aoc-day09/src/Main.hs
@@ -7,22 +7,19 @@ import Control.Monad.Trans.State.Lazy
 
 type Input = String
 
-part2 :: String -> ()
-part2 = const ()
-
-part1 :: String -> Int
-part1 = go 0
+solve :: String -> Int
+solve = go 0
   where go depth ('{':more) = go (depth + 1) more
-        go depth ('}':',':more) = go (depth - 1) more + depth
-        go depth ('}':more) = go (depth - 1) more + depth
+        go depth ('}':',':more) = go (depth - 1) more
+        go depth ('}':more) = go (depth - 1) more
         go depth ('<':more) = garbage more
           where garbage ('!':_:more) = garbage more
                 garbage ('>':',':more) = go depth more
                 garbage ('>':more) = go depth more
-                garbage (_:more) = garbage more
+                garbage (_:more) = garbage more + 1
                 garbage xs = error $ show (take 5 xs)
         go depth "" = depth
         go depth xs = error $ take 5 xs
 
 main :: IO ()
-main = interact $ show . (part1 &&& part2) . head . lines
+main = interact $ show . solve . head . lines
