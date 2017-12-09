5eb5c7b67c5192b0355cafa82921e507fb79d1c0
Alan Malloy <alan@malloys.org>
2017-12-09 05:22:35 +0000

    Day 9, part 1

--- aoc-day09/src/Main.hs
+++ aoc-day09/src/Main.hs
@@ -2,11 +2,27 @@ module Main where
 
 import Control.Arrow ((&&&))
 
-part1 :: a -> ()
-part1 = const ()
+import qualified Data.Map.Lazy as M
+import Control.Monad.Trans.State.Lazy
 
-part2 :: a -> ()
+type Input = String
+
+part2 :: String -> ()
 part2 = const ()
 
+part1 :: String -> Int
+part1 = go 0
+  where go depth ('{':more) = go (depth + 1) more
+        go depth ('}':',':more) = go (depth - 1) more + depth
+        go depth ('}':more) = go (depth - 1) more + depth
+        go depth ('<':more) = garbage more
+          where garbage ('!':_:more) = garbage more
+                garbage ('>':',':more) = go depth more
+                garbage ('>':more) = go depth more
+                garbage (_:more) = garbage more
+                garbage xs = error $ show (take 5 xs)
+        go depth "" = depth
+        go depth xs = error $ take 5 xs
+
 main :: IO ()
-main = interact $ show . (part1 &&& part2) . lines
+main = interact $ show . (part1 &&& part2) . head . lines
