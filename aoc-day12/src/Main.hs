4851a5660ed3a217556b356bf6644d67aa520d77
Alan Malloy <alan@malloys.org>
2017-12-12 07:20:57 +0000

    Day 12, part 1

--- aoc-day12/src/Main.hs
+++ aoc-day12/src/Main.hs
@@ -2,11 +2,59 @@ module Main where
 
 import Control.Arrow ((&&&))
 
-part1 :: a -> ()
-part1 = const ()
+import qualified Data.Map.Strict as M
+import Control.Monad.Trans.State.Lazy
+import Control.Monad (when)
 
-part2 :: a -> ()
+
+type Program = Int
+data ProgInfo = PI {parent :: Program,
+                    rank :: Int} deriving Show
+type Directory = (M.Map Program ProgInfo)
+type Lookup a = State Directory a
+
+
+mkSet :: Program -> Lookup ProgInfo
+mkSet p = do
+  modify $ M.insertWith (flip const) p (PI p 0)
+  gets (M.! p)
+
+find :: Program -> Lookup ProgInfo
+find p = do
+  pi@(PI parent rank) <- mkSet p
+  if parent == p
+    then pure pi
+    else do
+      root <- find parent
+      modify $ M.insert p root
+      pure root
+
+union :: Program -> Program -> Lookup ()
+union x y = do
+  xn@(PI xRoot xRank) <- find x
+  yn@(PI yRoot yRank) <- find y
+  when (xRoot /= yRoot) $ do
+    case compare xRank yRank of
+      LT -> modify $ M.insert xRoot yn
+      GT -> modify $ M.insert yRoot xn
+      EQ -> modify . M.union $ M.fromList [(yRoot, xn),
+                                           (xRoot, (PI xRoot (succ xRank)))]
+
+parse :: [String] -> [Program]
+parse (p:"<->":progs) = read <$> p : map (filter (/= ',')) progs
+
+flatten :: [[Program]] -> Directory
+flatten neighbors = execState go M.empty
+  where go = do
+          mapM_ process neighbors
+          mapM_ find =<< gets M.keys
+        process (x:xs) = mapM_ (union x) xs
+
+part1 :: Directory -> Int
+part1 m = length . filter ((== goal) . parent) . M.elems $ m
+  where (PI goal _) = m M.! 0
+
 part2 = const ()
 
 main :: IO ()
-main = interact $ show . (part1 &&& part2) . lines
+main = interact $ show . (part1 &&& part2) . flatten . map (parse . words) . lines
