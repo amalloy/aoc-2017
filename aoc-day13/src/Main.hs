d67da5c4234567170cfcf1646b64c358ff014a3f
Alan Malloy <alan@malloys.org>
2017-12-13 05:37:40 +0000

    Day 13, part 2

--- aoc-day13/src/Main.hs
+++ aoc-day13/src/Main.hs
@@ -14,21 +14,22 @@ data Firewall = Firewall Layer Range
 caught :: Range -> Time -> Bool
 caught r t = t `mod` ((r - 1) * 2) == 0
 
-cost :: Firewall -> Time -> Int
-cost (Firewall layer range) t | caught range t = (layer * range)
-                              | otherwise = 0
+cost :: Firewall -> Int
+cost (Firewall layer range) = layer * range
 
 parse :: String -> Firewall
 parse s = let (layer, (':':' ':range)) = break (== ':') s
           in Firewall (read layer) (read range)
 
-part1 :: [Firewall] -> Int
-part1 fws = sum [cost fw layer |
-                 fw@(Firewall layer range) <- fws,
-                 caught range layer]
+alarms :: Int -> [Firewall] -> [Firewall]
+alarms offset fws = [fw | fw@(Firewall layer range) <- fws,
+                     caught range (layer + offset)]
 
-part2 :: a -> ()
-part2 = const ()
+part1 :: [Firewall] -> Int
+part1 = sum . map cost . alarms 0
+
+part2 :: [Firewall] -> Int
+part2 fws = head [start | start <- [0..], null $ alarms start fws]
 
 main :: IO ()
 main = interact $ show . (part1 &&& part2) . map parse . lines
