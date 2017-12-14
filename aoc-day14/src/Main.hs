dd1375c61b0ad52744cc796b26a1eb0e0c733a21
Alan Malloy <alan@malloys.org>
2017-12-14 22:07:53 +0000

    Day 14, part 1

--- aoc-day14/src/Main.hs
+++ aoc-day14/src/Main.hs
@@ -1,14 +1,27 @@
 module Main where
 
 import KnotHash (knot)
+import Data.Bits
+import Data.Char (digitToInt)
 
 import Control.Arrow ((&&&))
 
-part1 :: a -> ()
-part1 = const ()
+bits :: Int -> [Bool]
+bits c = do
+  bit <- [3,2..0]
+  pure $ c .&. (1 `shiftL` bit) /= 0
+
+row :: String -> Int -> [Bool]
+row key idx = bits . digitToInt =<< knot (key ++ "-" ++ show idx)
+
+grid :: Int -> String -> [[Bool]]
+grid rows key = map (row key) [0..rows - 1]
+
+part1 :: [[Bool]] -> Int
+part1 = length . filter id . concat
 
 part2 :: a -> ()
 part2 = const ()
 
 main :: IO ()
-main = interact $ show . (part1 &&& part2) . lines
+main = interact $ show . (part1 &&& part2) . grid 128 . head . lines
