5e4e9b6f68e7c2f327d4c957f428acd3c9aecdff
Alan Malloy <alan@malloys.org>
2017-12-14 23:37:19 +0000

    Day 14, part 2
    An unsatisfying solution: I clearly have a space leak somewhere, since
    deepseq fixes it, but I can't figure out how to find the real problem.

--- aoc-day14/src/Main.hs
+++ aoc-day14/src/Main.hs
@@ -3,8 +3,15 @@ module Main where
 import KnotHash (knot)
 import Data.Bits
 import Data.Char (digitToInt)
+import Data.List (nub)
+import Data.Maybe (catMaybes)
+import Control.DeepSeq
+import qualified Data.Map.Strict as M
 
 import Control.Arrow ((&&&))
+
+type Coord a = (a, a)
+type Group = M.Map (Coord Int) Int
 
 bits :: Int -> [Bool]
 bits c = do
@@ -17,11 +24,34 @@ row key idx = bits . digitToInt =<< knot (key ++ "-" ++ show idx)
 grid :: Int -> String -> [[Bool]]
 grid rows key = map (row key) [0..rows - 1]
 
+coords :: [[Bool]] -> [Coord Int]
+coords gs = do
+  (y, row) <- zip [0..] gs
+  (x, True) <- zip [0..] row
+  pure (y, x)
+
+asMap :: [Coord Int] -> Group
+asMap = M.fromList . flip zip [0..]
+
+neighbors :: Num a => Coord a -> [Coord a]
+neighbors (y, x) = [(y + dy, x + dx) | (dy, dx) <- zip [0,0,1,-1] [1,-1,0,0]]
+
+spread :: Coord Int -> Group -> Group
+spread k m = m `deepseq` foldr relabel m (nub adjs)
+  where adjs = catMaybes . map (`M.lookup` m) $ neighbors k
+        new = m M.! k
+        relabel old = fmap replace
+          where replace x | x == old = new
+                          | otherwise = x
+
+coalesce :: Group -> Group
+coalesce m = foldr spread m (M.keys m)
+
 part1 :: [[Bool]] -> Int
 part1 = length . filter id . concat
 
-part2 :: a -> ()
-part2 = const ()
+part2 :: [[Bool]] -> Int
+part2 = length . nub . M.elems . coalesce . asMap . coords
 
 main :: IO ()
 main = interact $ show . (part1 &&& part2) . grid 128 . head . lines
