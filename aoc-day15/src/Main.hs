67ae20fee7dcf19a116be49b7bd047138658da9e
Alan Malloy <alan@malloys.org>
2017-12-17 02:18:26 +0000

    Day 15, part 1

--- aoc-day15/src/Main.hs
+++ aoc-day15/src/Main.hs
@@ -2,19 +2,15 @@ module Main where
 
 import Control.Arrow ((&&&))
 import Data.Function (on)
+import Data.Bits ((.&.))
 
-type States = ([Integer], [Integer])
+type Gens = (Integer, Integer)
 
 next :: Integer -> Integer -> Integer
-next seed curr = (seed * curr) `mod` 2147483647
+next curr factor = (factor * curr) `mod` 2147483647
 
-vals :: Integer -> Integer -> [Integer]
-vals = iterate . next
--- vals factor init = iterate (next factor) init
--- vals factor = iterate (next factor)
-
-part1 :: States -> Int
-part1 = length . filter id . take 40000000 . uncurry (zipWith ((==) `on` (`mod` 65536)))
+part1 :: Gens -> Int
+part1 (a, b) = length . filter id . take 40000000 . tail $ zipWith ((==) `on` (.&. 0xffff)) (iterate (next 16807) a) (iterate (next 48271) b)
 
 part2 :: a -> ()
 part2 = const ()
@@ -22,8 +18,8 @@ part2 = const ()
 parse :: String -> Integer
 parse = read . last . words
 
-puzzle :: [String] -> States
-puzzle [a,b] = (tail . vals 16807 $ parse a, tail . vals 48271 $ parse b)
+puzzle :: [String] -> Gens
+puzzle [a,b] = (parse a, parse b)
 
 main :: IO ()
 main = interact $ show . (part1 &&& part2) . puzzle . lines
