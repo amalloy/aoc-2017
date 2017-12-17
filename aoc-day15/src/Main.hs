418cab52798b0953e134b751a126e81e7f4f1436
Alan Malloy <alan@malloys.org>
2017-12-17 03:16:08 +0000

    Day 15, part 2

--- aoc-day15/src/Main.hs
+++ aoc-day15/src/Main.hs
@@ -7,13 +7,19 @@ import Data.Bits ((.&.))
 type Gens = (Integer, Integer)
 
 next :: Integer -> Integer -> Integer
-next curr factor = (factor * curr) `mod` 2147483647
+next factor curr = (factor * curr) `mod` 2147483647
+
+judge :: [Integer] -> [Integer] -> [Bool]
+judge = zipWith ((==) `on` (.&. 0xffff))
 
 part1 :: Gens -> Int
-part1 (a, b) = length . filter id . take 40000000 . tail $ zipWith ((==) `on` (.&. 0xffff)) (iterate (next 16807) a) (iterate (next 48271) b)
+part1 (a, b) = length . filter id . take 40000000 . tail $ judge (iterate (next 16807) a) (iterate (next 48271) b)
 
-part2 :: a -> ()
-part2 = const ()
+part2 :: Gens -> Int
+part2 (a, b) = length . filter id . take 5000000 $ judge as bs
+  where as = go 16807 4 a
+        bs = go 48271 8 b
+        go factor rem = filter ((== 0) . (`mod` rem)) . iterate (next factor)
 
 parse :: String -> Integer
 parse = read . last . words
