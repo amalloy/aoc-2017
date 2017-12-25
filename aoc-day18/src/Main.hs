0bb9bcb76eb7b565f1a4e68e90d47716b681684f
Alan Malloy <alan@malloys.org>
2017-12-25 03:59:38 +0000

    Day 18: redesign part 1
    It's now made of building blocks that will be useful for part 2 as well.

--- aoc-day18/src/Main.hs
+++ aoc-day18/src/Main.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import Prelude hiding (IO)
 import Control.Arrow ((&&&))
 import qualified Data.Map.Strict as M
 import Data.Maybe (catMaybes)
@@ -12,17 +13,17 @@ type Parser a = Parsec String () a
 type Register = Char
 data Arg = Reg Register | Lit Int deriving Show
 data Mutation = Set | Add | Mul | Mod deriving Show
-data Instruction = Sound Arg
-                 | Mutate Mutation Register Arg
-                 | Recover Arg
+data Instruction = Mutate Mutation Register Arg
+                 | Snd Arg
+                 | Rcv Arg
                  | JumpPos Arg Arg
                  deriving (Show)
 
+data IO = S Arg | R Arg deriving Show
+
 data Computer = Computer {instrs :: [Instruction],
                           ip :: Int,
-                          regs :: M.Map Register Int,
-                          sound :: Maybe Int,
-                          recovered :: Maybe Int}
+                          regs :: M.Map Register Int}
                 deriving (Show)
 
 mutate :: Mutation -> Int -> Int -> Int
@@ -35,22 +36,23 @@ eval :: Arg -> Computer -> Int
 eval (Lit x) c = x
 eval (Reg r) c = M.findWithDefault 0 r (regs c)
 
-runInstruction :: Computer -> Computer
-runInstruction c@(Computer prog ip regs prevSound recvd) =
+runUntilIO :: Computer -> (Computer, IO)
+runUntilIO c@(Computer prog ip regs) =
   let instr = prog !! ip
       nextIP = ip + case instr of
         JumpPos test offset | eval test c > 0 -> eval offset c
         _ -> 1
       state = case instr of
-        Sound arg -> c {sound = Just $ eval arg c}
-        Recover arg | eval arg c == 0 -> c
-                    | otherwise -> c {recovered = sound c}
         Mutate m r arg -> c {regs = M.insert r (mutate m
                                                 (eval arg c)
                                                 (eval (Reg r) c))
                                     regs}
-        JumpPos _ _ -> c
-  in state {ip = nextIP}
+        _ -> c
+      state' = state {ip = nextIP}
+  in case instr of
+    Snd arg -> (state', S arg)
+    Rcv arg -> (state', R arg)
+    _ -> runUntilIO state'
 
 int :: Parser Int
 int = do
@@ -73,8 +75,8 @@ instruction = mut "set" Set
           <|> mut "add" Add
           <|> mut "mul" Mul
           <|> mut "mod" Mod
-          <|> unary "snd" Sound
-          <|> unary "rcv" Recover
+          <|> unary "snd" Snd
+          <|> unary "rcv" Rcv
           <|> do
             string "jgz"
             spaces
@@ -87,11 +89,13 @@ program :: Parser [Instruction]
 program = instruction `endBy1` newline <* eof
 
 -- Less cluttered output, for visual debugging
-debug c = (instrs c !! ip c, regs c, sound c, recovered c)
+debug c = (instrs c !! ip c, regs c)
 
-part1 instructions = head . catMaybes . map recovered $ states
-  where states = iterate runInstruction $
-                 Computer instructions 0 M.empty Nothing Nothing
+part1 instructions = go Nothing $ Computer instructions 0 M.empty
+  where go sound c = case runUntilIO c of
+          (c', io@(S arg)) -> go (Just (eval arg c')) c'
+          (c', io@(R arg)) | eval arg c' /= 0 -> sound
+                           | otherwise -> go sound c'
 
 part2 :: a -> ()
 part2 = const ()
@@ -101,5 +105,4 @@ parseProgram s = case parse program "stdin" s of
   Left err -> error $ show err
   Right program -> program
 
-main :: IO ()
 main = interact $ show . (part1 &&& part2) . parseProgram
